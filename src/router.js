576011b17292e08323234c979b1b090e74d9ba35
yc-x <34796396+yc-x@users.noreply.github.com>
2022-08-28 17:52:16 -0700

    Add basic router structure

---
+++ src/router.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory} from 'vue-router';
+
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesDetail.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/', 
+            redirect: '/coaches',
+        },
+        {
+            path: '/coaches', 
+            component: CoachesList,
+        },
+        {
+            path: '/coaches/:id', 
+            component: CoachDetail,
+            children:[
+                {
+                    path: '/contact', 
+                    component: ContactCoach,
+                },
+            ],
+        },
+        {
+            path: '/register', 
+            component: CoachRegistration,
+        },
+        {
+            path: '/request', 
+            component: RequestReceived,
+        },     
+        {
+            path: '/:notFound(.*)',     // not found filtering some RegEx
+            component: NotFound,
+        },
+    ],
+});
+
+export default router;
